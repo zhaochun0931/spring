44ef9d5444eee88ed18bbcb8a68e4f4075e92a6c
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-06-07 21:40:25 +0800

    Update and rename DemoApplication.java to HelloworldApplication.java

--- HelloworldApplication.java
+++ HelloworldApplication.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class DemoApplication {
+public class HelloworldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
 	    @GetMapping("/hello")
